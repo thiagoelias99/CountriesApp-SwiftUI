9935facb1e1e8480394a800b76f0cb86f2a773e5
thiagoelias99 <thiagoelias99@gmail.com>
2023-10-12 08:24:33 -0400

    first commit

---
+++ CountriesApp SwiftUI/View/Components/RoundedTextField.swift
@@ -0,0 +1,20 @@
+//
+//  RoundedTextField.swift
+//  CountriesApp SwiftUI
+//
+//  Created by user241339 on 10/12/23.
+//
+
+import SwiftUI
+
+struct RoundedTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RoundedTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        RoundedTextField()
+    }
+}
