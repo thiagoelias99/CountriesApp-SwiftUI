f46edecb131717521502a15966582cf22c475aa4
thiagoelias99 <thiagoelias99@gmail.com>
2023-10-12 08:09:02 -0400

    Initial Commit

---
+++ CountriesApp SwiftUI/CountriesApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp_SwiftUIApp.swift
+//  CountriesApp SwiftUI
+//
+//  Created by user241339 on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
